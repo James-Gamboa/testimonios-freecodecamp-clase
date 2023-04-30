3915b9cf3dd77e7a38fd8ef346da50919e8b9f43
James <jjguevarag@gmail.com>
2023-04-30 12:14:04 -0600

    .

--- src/App.js
+++ src/App.js
@@ -32,25 +32,26 @@ const testimonios = [
   },
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <div className="contenedor-principal">
-        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        {testimonios.map((testimonio) => (
-          <Testimonio
-            key={testimonio.nombre}
-            nombre={testimonio.nombre}
-            pais={testimonio.pais}
-            imagen={testimonio.imagen}
-            cargo={testimonio.cargo}
-            empresa={testimonio.empresa}
-            testimonio={testimonio.testimonio}
-          />
-        ))}
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="contenedor-principal">
+          <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+          {testimonios.map((testimonio) => (
+            <Testimonio
+              key={testimonio.nombre}
+              nombre={testimonio.nombre}
+              pais={testimonio.pais}
+              imagen={testimonio.imagen}
+              cargo={testimonio.cargo}
+              empresa={testimonio.empresa}
+              testimonio={testimonio.testimonio}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
